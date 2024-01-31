91490e4a41f4c82deae1beb86bb3f2ff1abd61d5
Alina liu <119249419+Abnormalina@users.noreply.github.com>
2024-01-31 08:56:09 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -5,60 +5,40 @@ import java.util.Scanner; // Import the Scanner class to read text files
 public class PalindromeChecker {
 public void tester()
 {
- // String lines[] = loadStrings("palindromes.txt");
-  String lines[] = {"test", "rotator", "rewriter", "nurses run", "Madam, I'm Adam!", "A Man! A Plan! A Canal! Panama!"};
- // String[] lines = new String[6]; 
-    try{
-        File myFile = new File("palindromes.txt");
-        Scanner myReader = new Scanner(myFile);
-        int counter = 0;
-        while (myReader.hasNextLine()) {
-            String data = myReader.nextLine();
-            lines[counter] = data;
-            counter++;
-        }
-        myReader.close();
-    }
-    catch (FileNotFoundException e) {
-        System.out.println("An error occurred.");
-        e.printStackTrace();
-    }
+  String lines[] = loadStrings("palindromes.txt");
   System.out.println("there are " + lines.length + " lines");
   for (int i=0; i < lines.length; i++) 
   {
     if(palindrome(lines[i])==true)
     {
-      System.out.println("true");
+      System.out.println(lines[i] + " IS a palindrome.");
     }
     else
     {
-      System.out.println("false");
+      System.out.println(lines[i] + " is NOT a palindrome.");
     }
   }
 }
-public boolean palindrome(String sWord) {
-    String str = "";
-  for(int i = sWord.length()-1; i >=0; i--){
-      str += sWord.charAt(i);
+public String onlyLetters(String sString){
+ String str = "";
+  for(int i = 0; i < sString.length(); i++){
+    if(Character.isLetter(sString.charAt(i)) == true){
+      str += sString.charAt(i);
+    }
   }
-  str= str.toLowerCase();
-if (str.equals(reverse(sWord))){
-  return true;
+  return str;
 }
-return false;
+public boolean palindrome(String sWord) {
+ String jword = onlyLetters(sWord).toLowerCase();
+  String reversedWord = reverse(jword);
+  return jword.equals(reversedWord);
 }
 
 public String reverse(String word) {
   String str = new String();
   for (int i = word.length() - 1; i >= 0; i--) {
-    if(Character.isLetter(str.charAt(i))){
     str += word.charAt(i);
   }
-
-}
-str = str.toLowerCase();
-return str;
-  
+  return str;
 }
 }
-
